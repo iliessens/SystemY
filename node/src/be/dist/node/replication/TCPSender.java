3547d77f74cd6f56e7aea9ceea03c616f669c0e1
Imre Liessens <imre.liessens@telenet.be>
2017-11-20 12:10:18 +0100

    fixed sender filename format

--- node/src/be/dist/node/replication/TCPSender.java
+++ node/src/be/dist/node/replication/TCPSender.java
@@ -2,6 +2,7 @@ package be.dist.node.replication;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 
 public class TCPSender {
@@ -15,7 +16,6 @@ public class TCPSender {
     public void send(String ipOmMeeTeVerbinden, String fileName) {
         Socket senderSocket = null;
         try {
-
             System.out.println("CONNECTING ... ");
             senderSocket = new Socket(ipOmMeeTeVerbinden, port);
             System.out.println("[CONNECTION ESTABLISHED]");
@@ -24,11 +24,11 @@ public class TCPSender {
             OutputStream out = senderSocket.getOutputStream();
 
             byte[] buffer = new byte[1024];
-            byte[] nameSpace = new byte[256];
             int bytesRead;
             System.out.println("TRANSFERING FILE ... ");
-            nameSpace = fileName.getBytes("UTF-8");
-            out.write(fileName.getBytes("UTF-8"));
+            byte[] tempNameBytes = fileName.substring(fileName.lastIndexOf('/') + 1).getBytes("UTF-8"); // get only filename
+            byte[] nameSpace = Arrays.copyOf(tempNameBytes,256);
+            out.write(nameSpace);
             while ((bytesRead = in.read(buffer)) != -1) {
                 out.write(buffer, 0, bytesRead);
             }
