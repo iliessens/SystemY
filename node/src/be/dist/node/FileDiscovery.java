b9efc5b37f104441e4cb4d6aa4479b4bdbac3704
Imre Liessens <imre.liessens@telenet.be>
2017-11-20 11:08:09 +0100

    FileDiscovery verder afgewerkt.

--- node/src/be/dist/node/FileDiscovery.java
+++ node/src/be/dist/node/FileDiscovery.java
@@ -11,30 +11,40 @@ public class FileDiscovery {
     NamingServerInt remoteSetup;
     String myIP;
     String myName;
+
     public FileDiscovery(String ServerIP, String ip, String name){
         myIP = ip;
         myName = name;
         try {
             Registry registry = LocateRegistry.getRegistry(ServerIP);
             remoteSetup = (NamingServerInt) registry.lookup("ServerRepository");
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
+        } catch (RemoteException | NotBoundException e) {
             e.printStackTrace();
         }
     }
 
+    public void discoverFiles() {
+        FileIO io =  new FileIO();
+        for(String name : io.getLocalFiles()) {
+            fileCheck(name);
+        }
+    }
 
-    public void fileCheck(String fileName) throws RemoteException {
 
+    public void fileCheck(String fileName)  {
+        try {
             String fileOwner = remoteSetup.getOwner(fileName);
-            if(fileOwner.equals(myIP)){
+            if (fileOwner.equals(myIP)) {
                 String fileDuplicate = remoteSetup.getOwner(myName);
                 //sendFile to IP fileDuplicate
-            }
-            else{
+            } else {
                 //sendFile to IP fileOwner
             }
+        }
+        catch (RemoteException e) {
+            e.printStackTrace();
+            // doe failure handler
+        }
 
     }
 }
