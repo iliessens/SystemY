e2ebf2e65a395e39d9e572b7b8331e1f19e2b3bf
Imre Liessens <imre.liessens@telenet.be>
2017-11-22 11:13:27 +0100

    Temporarly disabled tests for TCP

--- node/test/TCPReceiverTest.java
+++ node/test/TCPReceiverTest.java
@@ -1,8 +1,10 @@
 import be.dist.node.replication.TCPListener;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TCPReceiverTest {
 
+    @Ignore
     @Test
     public void receiveFile() {
         TCPListener listener = new TCPListener(7899);
--- node/test/TCPSenderTest.java
+++ node/test/TCPSenderTest.java
@@ -1,11 +1,13 @@
 import be.dist.node.replication.TCPSender;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TCPSenderTest {
 
+    @Ignore
     @Test
     public void sendTestFile() {
-        TCPSender sender = new TCPSender(7896);
-        sender.send("127.0.0.1","files/original/testfile.txt");
+        TCPSender sender = new TCPSender(7899);
+        sender.send("10.2.1.15","files/original/testfile.txt");
     }
 }
