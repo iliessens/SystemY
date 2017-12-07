64875ea3a894b133f712386ed74c3dba647f5337
Imre Liessens <imre.liessens@telenet.be>
2017-12-07 10:32:59 +0100

    Getters and setter for LocalIP

--- node/src/be/dist/node/agents/LocalIP.java
+++ node/src/be/dist/node/agents/LocalIP.java
@@ -1,4 +1,14 @@
 package be.dist.node.agents;
 
 public class LocalIP {
+
+    private static String localIP;
+
+    public static void setLocalIP(String ip) {
+        localIP = ip;
+    }
+
+    public static String getLocalIP() {
+        return localIP;
+    }
 }
