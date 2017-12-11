38e24ae47c7a607a4b078f186d9cda06002af91d
Imre Liessens <imre.liessens@telenet.be>
2017-12-11 18:10:39 +0100

    Added observable capabilities to LocalFileList

--- node/src/be/dist/node/agents/LocalFileList.java
+++ node/src/be/dist/node/agents/LocalFileList.java
@@ -1,22 +1,29 @@
 package be.dist.node.agents;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class LocalFileList {
 
     private static Map<String,AgentFile> fileMap;
+    private static Set<Consumer<Set<String>>> observers = new HashSet<>();
 
     public LocalFileList() {
         fileMap = new HashMap<>();
     }
 
     public static Map<String, AgentFile> getFileMap() {
-        return fileMap;
+        return Collections.unmodifiableMap(fileMap); // prevent modifications to ensure observer pattern
     }
 
     public static void setFileMap(Map<String, AgentFile> fileMap) {
         LocalFileList.fileMap = fileMap;
+        // map changed, notify observers
+        observers.forEach(x -> x.accept(fileMap.keySet()));
+    }
+
+    public static void addObserver(Consumer<Set<String>> consumer) {
+        observers.add(consumer);
     }
 
     public static void lockFile(String filename) {
