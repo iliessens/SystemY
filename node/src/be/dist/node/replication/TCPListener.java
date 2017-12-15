6d3115bb89033ca04c0c1c4428660288cff62eeb
Imre Liessens <imre.liessens@telenet.be>
2017-12-15 10:18:09 +0100

    Allow TCP download path to be set in constructor.

--- node/src/be/dist/node/replication/TCPListener.java
+++ node/src/be/dist/node/replication/TCPListener.java
@@ -7,9 +7,15 @@ import java.net.Socket;
 public class TCPListener extends Thread {
 
     private int port = 7896;
+    private String filePath;
 
     public TCPListener(int port) {
+        this(port,"files/replication/");
+    }
+
+    public TCPListener(int port, String filePath) {
         this.port = port;
+        this.filePath = filePath;
     }
 
     @Override
@@ -20,7 +26,7 @@ public class TCPListener extends Thread {
             while (true) {
                 System.out.println("AWAITING CLIENTS ...");
                 Socket openedConnection = socket.accept();
-                Connection temp = new Connection(openedConnection);
+                Connection temp = new Connection(openedConnection,filePath);
 
             }
         } catch (IOException e) {
